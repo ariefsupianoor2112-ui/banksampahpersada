010214556547a93ebc927ad885fea62f6e4090db
ariefsupianoor2112-ui <arief.supianoor2112@gmail.com>
2026-07-30 19:44:36 +0800

    Implement HTTPS redirection in boot method
    Force HTTPS scheme in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,9 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
-    public function boot(): void
+  public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
-}
