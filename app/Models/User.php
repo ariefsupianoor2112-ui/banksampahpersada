f5a7358ee58a467b574635ad50f53937d20a7ad7
ariefsupianoor2112-ui <arief.supianoor2112@gmail.com>
2026-07-30 20:17:03 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'role', 'no_hp', 'alamat'])]
+#[Fillable(['kode', 'name', 'email', 'password', 'role', 'no_hp', 'alamat'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
